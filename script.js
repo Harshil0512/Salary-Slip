0e0fd3809fe00265d460dc331342fabef5c6e279
Harshil Khamar <73790584+Harshil0512@users.noreply.github.com>
2021-06-08 10:03:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let sub=document.querySelectorAll(".submit-btn");
 		let salary=document.getElementById('salary');
 		sub[0].addEventListener("click",submits1);
 		let n;
+		let count=1;
 		let content=document.querySelectorAll(".input");
 		function submits1()
 		{
@@ -14,6 +15,7 @@ let sub=document.querySelectorAll(".submit-btn");
 				content[0].style="display:none;";
 				content[1].style="display:flex";
 				sub[1].setAttribute("onClick","submits2()");
+				document.querySelectorAll(".header h1")[1].innerText="Employee "+(count++);
 			}
 			else
 			{
@@ -27,11 +29,12 @@ let sub=document.querySelectorAll(".submit-btn");
 			{
 				if(name.value!="" && isNaN(Number(name.value)) && !isNaN(Number(sno.value)) && Number(salary.value)>0)
 				{
+					document.querySelectorAll(".header h1")[1].innerText="Employee "+(count++);
 					n--;
 					let obj=new Object();
 					obj["Name"]=name.value;
-					obj["sno"]=sno.value;
-					obj["salary"]=salary.value;
+					obj["sno"]=Number(sno.value);
+					obj["salary"]=Number(salary.value);
 					salarySlip.push(obj);
 					name.value="";
 					sno.value="";
@@ -50,6 +53,7 @@ let sub=document.querySelectorAll(".submit-btn");
 		};
 		function display()
 		{
+			document.getElementsByTagName('section')[0].style="width:90%;";
 			let output=document.getElementsByClassName("output")[0];
 			output.style="display:grid;";
 			output.innerHTML="";
@@ -58,7 +62,7 @@ let sub=document.querySelectorAll(".submit-btn");
 			{
 				let hal=value["salary"]*5/100;
 				let tax=value["salary"]*4/100;
-				let netSalary=value["salary"]+hal-tax
+				let netSalary=value["salary"]+hal-tax;
 				output.innerHTML=output.innerHTML+'<div><table class="display" border="1px" cellspacing="0"><tr><th colspan="5">'+value["Name"]+'</th></tr><tr><td>Salary Number</td><td>Salary</td><td>House Allowance</td><td>Tax</td><td>Net Salary</td></tr><tr><td>'+value["sno"]+'</td><td>'+value["salary"]+'</td><td>'+hal+'</td><td>'+tax+'</td><td>'+netSalary+'</td></tr></table></div>';
 			});
 		}
